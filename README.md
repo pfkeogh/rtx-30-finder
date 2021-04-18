aa695cab6a988fa1130f7c53435638467185e8c2
Patrick Keogh <pkeogh@activecampaign.com>
2021-04-18 12:58:12 -0400

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# RTX 30 Finder
+
+*Work In Progress!*
+
+Looking for a Nvidia 30 series Card? Tired of manually looking through <website(s)> just to see pages of out of stock cards? Unwilling to pay resellers crazy markups? 
+Automate your quest to find a card.
+
+Scrapes for in stock cards and will return you the URL(s) of all stocked cards it may find!  
+
+[GitHub](https://github.com/pfkeogh/rtx-30-finder)
+
+### Usage
+
+```
+
+
+```  
+If a card is found to be in stock, its info will be be printed along with the URL for you to purchase it ASAP!  
+
+### Supported Websites:
+just NewEgg for now! 
+
+
+### Run this from your phone as an IOS Shortcut
+enable remote login // SSH
+System Preferences -> Sharing -> Enable Remote Login
+use "Run script over SSH"
+
---
+++ src/rtx30finder/main.py
@@ -0,0 +1,84 @@
+import requests
+from html.parser import HTMLParser
+
+
+class NeweggScraper(HTMLParser):
+
+    def error(self, message):
+        pass
+
+    def __init__(self, *, convert_charrefs=True):
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.item_grid = False
+        self.item_grid_depth = None
+        self.item_container = False
+        self.item_container_depth = None
+        self.depth = 0
+        self.items_encountered = 0
+        self.card_data = {}
+        self.card_urls = {}
+
+    def reset_page_vars(self):
+        self.item_grid = False
+        self.item_grid_depth = None
+        self.item_container = False
+        self.item_container_depth = None
+        self.depth = 0
+
+    def handle_starttag(self, tag, attrs):
+        self.depth += 1
+        for key, value in attrs:
+            if key == "class" and value == "item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell":
+                self.item_grid = True
+                self.item_grid_depth = self.depth
+            if self.item_grid and key == "class" and value == "item-container":
+                self.item_container += True
+                self.item_container_depth = self.depth
+                self.items_encountered += 1
+            if self.item_container and tag == "a" and key == "href" and self.depth == self.item_container_depth + 1:
+                self.card_urls[self.items_encountered] = value
+
+    def handle_endtag(self, tag):
+        if self.item_container_depth == self.depth:
+            self.item_container = False
+        if self.item_grid_depth == self.depth:
+            self.item_grid = False
+        self.depth -= 1
+
+    def handle_data(self, data):
+        index = self.items_encountered
+        if self.item_container and self.item_grid:
+            if self.card_data.get(index) is None:
+                self.card_data[index] = data
+            else:
+                self.card_data[index] += data
+
+    def search(self, search_file=None):
+
+        for page in [1, 2, 3]:
+            if search_file is None:
+                response = requests.get(url=f"https://www.newegg.com/p/pl?N=100007709%20601357282&page={page}")
+                search_text = response.text
+            else:
+                file = open(search_file, 'r')
+                search_text = file.read()
+            self.feed(search_text)
+
+            print(f"items:{self.items_encountered}")
+            for key, val in self.card_data.items():
+                if "OUT OF STOCK" not in val:
+                    print(f"Stocked!: {val}")
+                    print(f"HURRY - go to: {self.card_urls[key]}")
+            self.reset_page_vars()
+
+
+def check_newegg():
+    path = "/Users/patrickkeogh/Library/Application Support/JetBrains/PyCharm2020.2/scratches/scratch.html"
+    scraper = NeweggScraper()
+    scraper.search()
+
+
+if __name__ == '__main__':
+    check_newegg()
+
+
